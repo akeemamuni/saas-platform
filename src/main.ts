99bf021a75930bb6c936eb9800b3ddaefec2e9a5
akeemamuni <amuni.engr@gmail.com>
2025-04-25 06:07:33 -0400

    Utilized ConfigService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    const port = app.get(ConfigService).get<number>('PORT') ?? 3000;
+    await app.listen(port, () => console.log(`Listening on port ${port}`));
+    //   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+// Add timestamp to personal console logs
