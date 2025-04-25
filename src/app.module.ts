a83cecc527a40894342c77ac821d3014d4737610
akeemamuni <amuni.engr@gmail.com>
2025-04-25 06:05:17 -0400

    Added config, auth, and prisma modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './shared/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env'
+    }),
+    AuthModule,
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
