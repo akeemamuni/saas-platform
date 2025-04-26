547fa464ef38957156f3e752b3522dcd0e0f88c3
akeemamuni <amuni.engr@gmail.com>
2025-04-26 03:01:49 -0400

    Strategy added to providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
+// import { JwtModule } from 'src/shared/jwt/jwt.module';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+    // imports: [JwtModule],
+    controllers: [AuthController],
+    providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
