2d89b03dd1c47bd9cc973b53c55414371ec1ffbf
akeemamuni <amuni.engr@gmail.com>
2025-04-26 03:00:58 -0400

    Strategy and guard

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from 'src/types/payload.type';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly config: ConfigService) {
+    // Extract secret from env
+    const secret = config.get<string>('SECRET');
+    if (!secret) throw new Error('SECRET not found..');
+
+    super({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: secret,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return payload;
+  }
+}
