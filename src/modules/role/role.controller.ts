1bd8608bef25bb9a9c98031fd588ae7a2bef13aa
akeemamuni <amuni.engr@gmail.com>
2025-04-29 22:19:04 -0400

    Updated Role and Plan Modules

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,17 +1,14 @@
-import { Body, Controller, Post, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { RoleService } from './role.service';
-import { CreateRoleDTO } from './dto/create-role.dto';
-import { UpdateRoleDTO } from './dto/update-role.dto';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 
 @Controller('role')
 export class RoleController {
     constructor(private readonly roleService: RoleService) {}
 
-    @Post()
+    @Get()
     @UseGuards(JwtAuthGuard)
-    create(@Body(ValidationPipe) createRoleDto: CreateRoleDTO) {
-        return this.roleService.create(createRoleDto);
+    findAll() {
+        return this.roleService.findAll();
     }
-    
 }
--- src/modules/role/role.service.ts
+++ src/modules/role/role.service.ts
@@ -1,36 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/shared/prisma/prisma.service';
-import { CreateRoleDTO } from './dto/create-role.dto';
-import { UpdateRoleDTO } from './dto/update-role.dto';
 
 @Injectable()
 export class RoleService {
     constructor(private readonly prisma: PrismaService) {}
 
-    async create(data: CreateRoleDTO) {
-        return this.prisma.role.create({ data });
-    }
-
     async findAll() {
         return this.prisma.role.findMany();
-    }
-
-    async findOne(id: string) {
-        const role = await this.prisma.role.findUnique({ where: { id } });
-        if (!role) throw new NotFoundException('Role not found');
-        return role;
-    }
-
-    async update(id: string, data: UpdateRoleDTO) {
-        await this.findOne(id);
-        return this.prisma.role.update({
-            where: { id },
-            data,
-        });
-    }
-
-    async remove(id: string) {
-        await this.findOne(id);
-        return this.prisma.role.delete({ where: { id } });
     }
 }
