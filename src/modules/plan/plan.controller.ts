8f1dee3c3260ab437330772cb2910e3d600eee58
akeemamuni <amuni.engr@gmail.com>
2025-05-01 01:08:24 -0400

    Added RBAC

--- src/modules/plan/plan.controller.ts
+++ src/modules/plan/plan.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PlanService } from './plan.service';
 import { JwtAuthGuard } from '../../shared/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/shared/guards/roles.guard';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { RoleType } from '@prisma/client';
 
 @Controller('plan')
 export class PlanController {
     constructor(private readonly planService: PlanService) {}
 
     @Get()
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(RoleType.ADMIN)
     findAll() {
         return this.planService.findAll()
     }
--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { JwtAuthGuard } from '../../shared/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/shared/guards/roles.guard';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { RoleType } from '@prisma/client';
 
 @Controller('role')
 export class RoleController {
     constructor(private readonly roleService: RoleService) {}
 
     @Get()
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(RoleType.ADMIN)
     findAll() {
         return this.roleService.findAll();
     }
