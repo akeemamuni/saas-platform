10728f3abfc0b1030e27cc19d7de62e44fc12a48
akeemamuni <amuni.engr@gmail.com>
2025-05-02 04:17:48 -0400

    Billing controller

---
+++ src/modules/billing/billing.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { BillingService } from './billing.service';
+import { JwtAuthGuard } from 'src/shared/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/shared/guards/roles.guard';
+import { Roles } from 'src/shared/decorators/roles.decorator';
+import { RoleType } from '@prisma/client';
+import { AuthUser } from 'src/shared/decorators/auth-user.decorator';
+import { JwtPayload } from 'src/shared/types/payload.type';
+
+@Controller('billing')
+export class BillingController {
+  constructor(private readonly billingService: BillingService) {}
+
+  @Get(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(RoleType.ADMIN)
+  create(@AuthUser() authUser: JwtPayload, @Param('id') planId: string) {
+    return this.billingService.createCheckoutSession(authUser, planId);
+  }
+}
