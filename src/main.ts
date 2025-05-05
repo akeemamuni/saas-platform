9434b74cae6d927a8a095b216561ae3bef631e65
akeemamuni <amuni.engr@gmail.com>
2025-05-04 23:14:00 -0400

    Dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
+dotenv.config();
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const port = app.get(ConfigService).get<number>('PORT') ?? 3000;
