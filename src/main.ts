6948ace4db934db7d26f2536b67577f27551bcbe
akeemamuni <amuni.engr@gmail.com>
2025-05-07 02:19:15 -0400

    Raw parsing of webhook route

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv';
+import * as bodyParser from 'body-parser';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
@@ -7,6 +8,7 @@ dotenv.config();
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const port = app.get(ConfigService).get<number>('PORT') ?? 3000;
+    app.use('/billing/webhook', bodyParser.raw({type: 'application/json'}));
     await app.listen(port, () => console.log(`Listening on port ${port}`));
     //   await app.listen(process.env.PORT ?? 3000);
 }
