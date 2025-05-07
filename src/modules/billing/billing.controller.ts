d2453b041ff33bd67602d46be685f157f04c0d06
akeemamuni <amuni.engr@gmail.com>
2025-05-07 02:20:53 -0400

    Webhook route setup

--- src/modules/billing/billing.controller.ts
+++ src/modules/billing/billing.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards, Post, Req } from '@nestjs/common';
 import { BillingService } from './billing.service';
 import { JwtAuthGuard } from 'src/shared/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/shared/guards/roles.guard';
@@ -6,15 +6,21 @@ import { Roles } from 'src/shared/decorators/roles.decorator';
 import { RoleType } from '@prisma/client';
 import { AuthUser } from 'src/shared/decorators/auth-user.decorator';
 import { JwtPayload } from 'src/shared/types/payload.type';
+import { RequestRawBody } from 'src/shared/decorators/raw-body.decorator';
 
 @Controller('billing')
 export class BillingController {
-  constructor(private readonly billingService: BillingService) {}
+    constructor(private readonly billingService: BillingService) {}
 
-  @Get('checkout/:planId')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(RoleType.ADMIN)
-  create(@AuthUser() authUser: JwtPayload, @Param('planId') planId: string) {
-    return this.billingService.createCheckoutSession(authUser, planId);
-  }
+    @Get('checkout/:planId')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(RoleType.ADMIN)
+    create(@AuthUser() authUser: JwtPayload, @Param('planId') planId: string) {
+        return this.billingService.createCheckoutSession(authUser, planId);
+    }
+
+    @Post('webhook')
+    stripeWebhook(@RequestRawBody() rawBody: Buffer, @Req() req: Request) {
+        return this.billingService.handleWebhook(req, rawBody);
+    }
 }
