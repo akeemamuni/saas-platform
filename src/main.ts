8f7acb402b2a75174416205b6b4294e73c370c1e
akeemamuni <amuni.engr@gmail.com>
2025-05-09 19:34:07 -0400

    CORS enabled

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import { AppModule } from './app.module';
 
 dotenv.config();
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, { cors: true });
     const port = app.get(ConfigService).get<number>('PORT') ?? 3000;
     app.use('/billing/webhook', bodyParser.raw({type: 'application/json'}));
-    await app.listen(port, () => console.log(`Listening on port ${port}`));
+    await app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}`));
     //   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
