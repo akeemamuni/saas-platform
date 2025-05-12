ec91b0464642dde9756962df3f94782624c9ad9d
akeemamuni <amuni.engr@gmail.com>
2025-05-12 02:43:46 -0400

    Email processor starter

---
+++ src/shared/job/welcome-email.processor.ts
@@ -0,0 +1,10 @@
+import { Processor } from "@nestjs/bullmq";
+import { Job } from "bullmq";
+
+// @Processor('email')
+// export class WelcomeEmailProcessor {
+//     @Process('send-welcome-email')
+//     async handleJob(job: Job) {
+
+//     }
+// }
