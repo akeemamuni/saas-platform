6b82d44aee5e5436b292cc6f370ff6502d3f9816
akeemamuni <amuni.engr@gmail.com>
2025-05-13 01:26:51 -0400

    Job module added to auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from '../../shared/strategies/jwt.strategy';
+import { JobModule } from 'src/shared/job/job.module';
 // import { JwtModule } from 'src/shared/jwt/jwt.module';
 
 @Module({
-    // imports: [JwtModule],
+    // imports: [JwtModule] -- Already made global
+    imports: [JobModule],
     controllers: [AuthController],
     providers: [AuthService, JwtStrategy],
 })
