2c3a3838b1c25a356fbd47a0adf5b54efc992287
akeemamuni <amuni.engr@gmail.com>
2025-05-13 01:19:32 -0400

    Changed queue setup

--- src/shared/job/job-queue.service.ts
+++ src/shared/job/job-queue.service.ts
@@ -8,13 +8,21 @@ export class JobQueueService {
     private readonly emailQueue: Queue;
 
     constructor(private readonly config: ConfigService) {
-        const redisUrl = this.config.get<string>('REDIS_URL');
-        if (!redisUrl) throw new Error('Redis URL not defined...');
-        this.emailQueue = new Queue('email', { connection: { host: redisUrl } });
+        const redisHost = this.config.get<string>('REDIS_HOST');
+        const redisPort = this.config.get<string>('REDIS_PORT');
+        if (!redisHost || !redisPort) throw new Error('Redis URL not defined...');
+        this.emailQueue = new Queue('email', {
+            connection: { host: redisHost, port: parseInt(redisPort) }
+        });
     }
 
     // Add welcome email job to queue
     async welcomeEmailJob(data: { email: string; name: string }) {
         await this.emailQueue.add('send-welcome-email', data);
+    }
+
+    // Add password reset to queue
+    async passwordResetEmailJob(email: string) {
+        await this.emailQueue.add('password-reset-email', email);
     }
 }
