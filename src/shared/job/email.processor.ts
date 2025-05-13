f05232b076d92257d8f311c317c16fceda4f70f7
akeemamuni <amuni.engr@gmail.com>
2025-05-13 01:24:22 -0400

    Worker process and job handling

---
+++ src/shared/job/email.processor.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { Worker, Job } from "bullmq";
+import { welcomeEmail } from "./jobs/welcome-email.job";
+import { passwordResetEmail } from "./jobs/password-reset.job";
+
+@Injectable()
+export class EmailProcessor {
+    private worker: Worker;
+    private readonly redisHost: string;
+    private readonly redisPort: number;
+
+    constructor(private readonly config: ConfigService) {
+        this.redisHost = this.config.get('REDIS_HOST') as string;
+        this.redisPort = Number(this.config.get('REDIS_PORT'));
+
+        if (!this.redisHost || !this.redisPort) {
+            throw new Error('Redis host and/or port not found...');
+        }
+    }
+
+    // Job processor
+    async handleJob(job: Job) {
+        switch(job.name) {
+            case 'send-welcome-email': return welcomeEmail(job.data);
+            case 'password-reset-email': return passwordResetEmail(job.data);
+            default: console.log(`Unhandled job: ${job.name}`);
+        }
+    }
+
+    // Start worker and process jobs
+    async start() {
+        this.worker = new Worker(
+            'email', async (job: Job) => this.handleJob(job),
+            {connection: {host: this.redisHost, port: this.redisPort}}
+        );
+
+        this.worker.on('completed', (job) => {
+            console.log(`Job ${job.name} completed`);
+        });
+
+        this.worker.on('failed', (job, err) => {
+            console.error(`Job ${job?.name} failed:`, err);
+        });
+    }
+
+    // Graceful shutdown
+    async stop() {
+        await this.worker?.close();
+    }
+}
