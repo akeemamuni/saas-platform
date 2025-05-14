6c9f6337d85a821b87eaa35d222279fdf287ec3a
akeemamuni <amuni.engr@gmail.com>
2025-05-14 02:36:20 -0400

    Job module added to billing module

--- src/modules/billing/billing.module.ts
+++ src/modules/billing/billing.module.ts
@@ -1,10 +1,12 @@
 import { Module, Global } from '@nestjs/common';
 import { BillingService } from './billing.service';
 import { BillingController } from './billing.controller';
+import { JobModule } from 'src/shared/job/job.module';
 
 @Global()
 @Module({
-  controllers: [BillingController],
-  providers: [BillingService],
+    imports: [JobModule],
+    controllers: [BillingController],
+    providers: [BillingService]
 })
 export class BillingModule {}
