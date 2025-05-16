70a46c6b47b36ab75cdef52bdf35cefe487e802d
akeemamuni <amuni.engr@gmail.com>
2025-05-16 00:38:10 -0400

    Mailer module added to job module

--- src/shared/job/job.module.ts
+++ src/shared/job/job.module.ts
@@ -1,8 +1,10 @@
 import { Module, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { JobQueueService } from './job-queue.service';
 import { EmailProcessor } from './email.processor';
+import { MailerModule } from '../mailer/mailer.module';
 
 @Module({
+    imports: [MailerModule],
     providers: [JobQueueService, EmailProcessor],
     exports: [JobQueueService]
 })
