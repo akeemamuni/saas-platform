7f16f2e45dab69c11c0f8ab556151363811d88b9
akeemamuni <amuni.engr@gmail.com>
2025-05-16 00:40:33 -0400

    Email jobs utilizing mailer service

--- src/shared/job/jobs/password-reset.job.ts
+++ src/shared/job/jobs/password-reset.job.ts
@@ -1,4 +1,5 @@
+import { MailerService } from "src/shared/mailer/mailer.service";
 
-export async function passwordResetEmail(email: string) {
+export async function passwordResetEmail(email: string, mailer: MailerService) {
     console.log(`Sending password reset email to ${email}`);
 }
--- src/shared/job/jobs/welcome-email.job.ts
+++ src/shared/job/jobs/welcome-email.job.ts
@@ -1,4 +1,9 @@
+import { MailerService } from "src/shared/mailer/mailer.service";
 
-export async function welcomeEmail(data: any) {
-    console.log(`Sending welcome email with data: ${data}`);
+export async function welcomeEmail(data: any, mailer: MailerService) {
+    console.log(`Sending welcome email with data: ${data.email}`);
+    const { email } = data;
+    const subject = 'Welcome To Our Platform';
+    const html = '<h2>Welcome to our unique platform, we are delighted...</h2>'
+    console.log(mailer.sendMail(email, subject, html));
 }
