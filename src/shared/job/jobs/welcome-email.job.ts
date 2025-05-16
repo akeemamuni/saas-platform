59f8d5b4af0a01aafc851c4258b7c243feb2e5da
akeemamuni <amuni.engr@gmail.com>
2025-05-16 01:25:58 -0400

    Email jobs utilizing mailer service

--- src/shared/job/jobs/welcome-email.job.ts
+++ src/shared/job/jobs/welcome-email.job.ts
@@ -1,9 +1,9 @@
 import { MailerService } from "src/shared/mailer/mailer.service";
 
 export async function welcomeEmail(data: any, mailer: MailerService) {
-    console.log(`Sending welcome email with data: ${data.email}`);
     const { email } = data;
     const subject = 'Welcome To Our Platform';
-    const html = '<h2>Welcome to our unique platform, we are delighted...</h2>'
-    console.log(mailer.sendMail(email, subject, html));
+    const html = '<h2>Welcome to our unique platform, we are delighted...</h2>';
+    const result = await mailer.sendMail(email, subject, html);
+    console.log(`Sending welcome email to ${data.email}\n${result}`);
 }
