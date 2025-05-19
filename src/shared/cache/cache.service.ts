9910618cc533bd2f3ca9a7e5271cee8134f12214
akeemamuni <amuni.engr@gmail.com>
2025-05-19 00:24:49 -0400

    Actual caching implementation

--- src/shared/cache/cache.service.ts
+++ src/shared/cache/cache.service.ts
@@ -1,22 +1,23 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class CacheService {
     constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
     
     // Get cached data
-    async getData<T>(key: string): Promise<T | null> {
-        return this.cache.get<T>(key);
-    }
-
-    // Get hashed token
-    async getHashedToken(key: string): Promise<string | null> {
+    async get<T>(key: string): Promise<T | null> {
         return this.cache.get(key);
     }
 
+    // Get hashed token
+    // async getHashedToken(key: string): Promise<string | null> {
+    //     return this.cache.get(key);
+    // }
+
     // Add data to cache
-    async setData<T>(key: string, value: T, ttl?: number): Promise<void> {
+    async set<T>(key: string, value: T, ttl?: number): Promise<void> {
         if (ttl) {
             await this.cache.set(key, value, ttl);
         } else {
@@ -25,13 +26,13 @@ export class CacheService {
     }
 
     // Add hashed token to cache
-    async setHashedToken(key: string, value: string, ttl?: number): Promise<void> {
-        if (ttl) {
-            await this.cache.set(key, value, ttl);
-        } else {
-            await this.cache.set(key, value, 120000);
-        }
-    }
+    // async setHashedToken(key: string, value: string, ttl?: number): Promise<void> {
+    //     if (ttl) {
+    //         await this.cache.set(key, value, ttl);
+    //     } else {
+    //         await this.cache.set(key, value);
+    //     }
+    // }
 
     // Delete data
     async deleteData(key:string): Promise<void> {
